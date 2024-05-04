dafb5a8ce4734898568ca1bffb11b958d68f1afc
Mateo Ap <mateo.aponte@masivian.com>
2024-05-04 00:08:42 -0500

    feat: Add back button

--- src/components/Home/SelectedPicture.jsx
+++ src/components/Home/SelectedPicture.jsx
@@ -1,4 +1,5 @@
 import { MediaPlayer } from './MediaPlayer';
+import Atras from '../../assets/img/icons/atras-icon.png';
 
 export const SelectedPicture = ({ children, selected, setSelected, setShowOverlay, className }) => {
   const closeSelection = () => {
@@ -9,6 +10,9 @@ export const SelectedPicture = ({ children, selected, setSelected, setShowOverla
   };
   return (
     <section className="gallery-media">
+      <div className="gallery-media__icon" onClick={() => closeSelection()}>
+        <img src={Atras} alt="Get to home view" />
+      </div>
       {children}
       <div className="gallery-artist" smooth-parallax="" start-position="0" end-position="1">
         <div>
@@ -16,7 +20,7 @@ export const SelectedPicture = ({ children, selected, setSelected, setShowOverla
             <MediaPlayer url={selected.url} className={className} />
           </div>
         </div>
-        <div className="home__gallery-selected-closed" onClick={closeSelection}>
+        <div className="home__gallery-selected-closed" onClick={() => closeSelection()}>
           X
         </div>
       </div>
